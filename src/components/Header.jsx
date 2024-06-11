3a656706c27edd252723e56fc38bdacaf973807a
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-06-11 14:54:10 +0100

    adds chakra UI and styles header adn SeriesInfo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
+import { Flex, Heading, Icon, Spacer } from "@chakra-ui/react"
+import { FaStethoscope } from "react-icons/fa6";
 function Header() {
-    return <header className="border"><h1>Grey's Anatomy</h1></header>
+    return <header>
+            <Flex justifyContent="center" alignItems="center" m={2}>
+                <Icon as={FaStethoscope} boxSize={16} justifySelf="start"  />
+                <Spacer />
+                <Heading as="h1" size="3xl" textAlign="center" p={4}>
+                    Grey's Anatomy
+                </Heading>
+                <Spacer />
+                <Spacer />
+            </Flex>
+        </header>
 }
 
 export default Header
--- src/components/SeriesInfo.jsx
+++ src/components/SeriesInfo.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Flex, Heading, Text, Image, Box } from "@chakra-ui/react";
 import { fetchSeries } from "../utils/api";
 import { stripHtmlTags } from "../utils/helpers";
 
@@ -10,26 +11,42 @@ function SeriesInfo() {
     }, []);
 
     return (
-        <article className="border flex-container column">
-            <h2>About Grey's Anatomy</h2>
-            <div className="flex-container">
-                {seriesData ? (
-                    <>
-                        <div className="flex-container column">
-                            <p>{stripHtmlTags(seriesData.summary)}</p>
-                            <p><strong>Genres:</strong> {seriesData.genres.join(' | ')}</p>
-                            <p><strong>Rating: </strong>{seriesData.rating.average}</p>
-                            <p><strong>Running since:</strong> {seriesData.premiered}</p>
-                        </div>
-                        <img
-                            src={seriesData.image.medium}
-                            alt="Head and shoulders images of Grey's Anatomy characters arranged inside the number 20, celebrating the show's 20th season."
-                        />
-                    </>
-                ) : (
-                    <p>Loading...</p>
-                )}
-            </div>
+        <article>
+            <Flex direction="column" p={4} mx={2} border='2px' borderColor='gray.400'>
+                <Heading as="h2" size="xl" p={4}>
+                    About Grey's Anatomy
+                </Heading>
+                <Flex>
+                    {seriesData ? (
+                        <>
+                            <Flex direction="column" px={4}>
+                                <Text fontSize="lg" py={4}>
+                                    {stripHtmlTags(seriesData.summary)}
+                                </Text>
+                                <Text fontSize="md" py={2}>
+                                    <Text as="span" fontWeight="bold" >Genres: </Text>{seriesData.genres.join(" | ")}
+                                </Text>
+                                <Text fontSize="md" py={2}>
+                                    <Text as="span" fontWeight="bold" >Rating: </Text>{seriesData.rating.average}
+                                </Text>
+                                <Text fontSize="md" py={2}>
+                                    <Text as="span" fontWeight="bold" >Running since:</Text> {seriesData.premiered}
+                                </Text>
+                            </Flex>
+                            <Image
+                                src={seriesData.image.medium}
+                                alt="Head and shoulders images of Grey's Anatomy characters arranged inside the number 20, celebrating the show's 20th season."
+                                boxSize="100%"
+                                maxW="10%"
+                                minW="200px"
+                                aspectRatio={3 / 4}
+                            />
+                        </>
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+                </Flex>
+            </Flex>
         </article>
     );
 }
