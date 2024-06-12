7f9a87bded04cf27170b6fb3dafdb6ee97c58be9
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-06-12 07:51:29 +0100

    refactors handle next and prev logic into getCards function

--- src/components/CharacterDisplay.jsx
+++ src/components/CharacterDisplay.jsx
@@ -1,75 +1,87 @@
 import { useEffect, useState } from "react";
-import { Flex, Heading, Button, ButtonGroup, Text } from "@chakra-ui/react";
-import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
+import { Flex, Heading, Button, ButtonGroup } from "@chakra-ui/react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
 import { useTheme } from "@chakra-ui/react";
 import CharacterCard from "./CharacterCard";
 import { fetchCast } from "../utils/api";
 
 function CharacterDisplay() {
     const [characters, setCharacters] = useState([]);
-    const [firstCharacter, setFirstCharacter] = useState(0)
-    const [lastCharacter, setLastCharacter] = useState(4)
+    const [firstCharacter, setFirstCharacter] = useState(0);
+    const [lastCharacter, setLastCharacter] = useState(4);
 
     useEffect(() => {
         fetchCast().then((castData) => {
-            setCharacters(castData)
-        })
-    }, [])
+            setCharacters(castData);
+        });
+    }, []);
 
-    const theme = useTheme()
+    const theme = useTheme();
 
-    const handleNext = () => {
+    const updateIndices = (step) => {
         setFirstCharacter((prevFirst) => {
-            const newFirst = (prevFirst + 4 + characters.length) % characters.length;
-            console.log(newFirst, 'first (next)')
-            return newFirst
-        })
+            const newFirst =
+                (prevFirst + step + characters.length) % characters.length;
+            return newFirst;
+        });
         setLastCharacter((prevLast) => {
-            const newLast = (prevLast + 4 + characters.length) % characters.length;
-            console.log(newLast, 'last (next)')
-            return newLast
-        })
-    }
+            const newLast =
+                (prevLast + step + characters.length) % characters.length;
+            return newLast;
+        });
+    };
 
-    const handlePrevious = () => {
-        setFirstCharacter((prevFirst) => {
-            const newFirst = (prevFirst - 4 + characters.length) % characters.length;
-            console.log(newFirst, 'first (prev)')
-            return newFirst
-        })
-        setLastCharacter((prevLast) => {
-            const newLast = (prevLast - 4 + characters.length) % characters.length;
-            console.log(newLast, 'last (prev)')
-            return newLast
-        })
-    }
+    const handleNext = () => updateIndices(4);
+    const handlePrevious = () => updateIndices(-4);
 
-    const getCards =  () => {
+    const getCards = () => {
         if (firstCharacter < lastCharacter) {
             return characters.slice(firstCharacter, lastCharacter);
         }
-        return [...characters.slice(firstCharacter), ...characters.slice(0, lastCharacter)];
-    }
+        return [
+            ...characters.slice(firstCharacter),
+            ...characters.slice(0, lastCharacter),
+        ];
+    };
 
-    return <section >
-        <Flex direction="column"
+    return (
+        <section>
+            <Flex
+                direction="column"
                 p={4}
                 mx={2}
                 my={4}
                 border="2px"
-                borderColor={theme.colors.custom.dark }>
-            <Heading as="h2">Characters</Heading>
-            <Flex>
-                {getCards().map((character) => (
-                        <CharacterCard key={character.character.id} character={character} />
+                borderColor={theme.colors.custom.dark}
+            >
+                <Heading as="h2">Characters</Heading>
+                <Flex>
+                    {getCards().map((character) => (
+                        <CharacterCard
+                            key={character.character.id}
+                            character={character}
+                        />
                     ))}
+                </Flex>
+                <ButtonGroup variant="outline" spacing="80%" mt={4}>
+                    <Button
+                        onClick={handlePrevious}
+                        borderColor={theme.colors.custom[700]}
+                        leftIcon={<ArrowLeftIcon />}
+                    >
+                        Prev
+                    </Button>
+                    <Button
+                        onClick={handleNext}
+                        borderColor={theme.colors.custom[700]}
+                        rightIcon={<ArrowRightIcon />}
+                    >
+                        Next
+                    </Button>
+                </ButtonGroup>
             </Flex>
-            <ButtonGroup variant="outline" spacing="80%" mt={4}>
-                <Button onClick={handlePrevious} borderColor={theme.colors.custom[700]} leftIcon={<ArrowLeftIcon />} >Prev</Button>
-                <Button onClick={handleNext} borderColor={theme.colors.custom[700]} rightIcon={<ArrowRightIcon />} >Next</Button>
-            </ButtonGroup>
-        </Flex>
-    </section>
+        </section>
+    );
 }
 
-export default CharacterDisplay
+export default CharacterDisplay;
