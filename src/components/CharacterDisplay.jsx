520670e4983958cfd61962831f7f78c7995223c1
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-06-12 07:45:49 +0100

    adds prev/next button functionality to character display

--- src/components/CharacterDisplay.jsx
+++ src/components/CharacterDisplay.jsx
@@ -7,6 +7,8 @@ import { fetchCast } from "../utils/api";
 
 function CharacterDisplay() {
     const [characters, setCharacters] = useState([]);
+    const [firstCharacter, setFirstCharacter] = useState(0)
+    const [lastCharacter, setLastCharacter] = useState(4)
 
     useEffect(() => {
         fetchCast().then((castData) => {
@@ -15,6 +17,39 @@ function CharacterDisplay() {
     }, [])
 
     const theme = useTheme()
+
+    const handleNext = () => {
+        setFirstCharacter((prevFirst) => {
+            const newFirst = (prevFirst + 4 + characters.length) % characters.length;
+            console.log(newFirst, 'first (next)')
+            return newFirst
+        })
+        setLastCharacter((prevLast) => {
+            const newLast = (prevLast + 4 + characters.length) % characters.length;
+            console.log(newLast, 'last (next)')
+            return newLast
+        })
+    }
+
+    const handlePrevious = () => {
+        setFirstCharacter((prevFirst) => {
+            const newFirst = (prevFirst - 4 + characters.length) % characters.length;
+            console.log(newFirst, 'first (prev)')
+            return newFirst
+        })
+        setLastCharacter((prevLast) => {
+            const newLast = (prevLast - 4 + characters.length) % characters.length;
+            console.log(newLast, 'last (prev)')
+            return newLast
+        })
+    }
+
+    const getCards =  () => {
+        if (firstCharacter < lastCharacter) {
+            return characters.slice(firstCharacter, lastCharacter);
+        }
+        return [...characters.slice(firstCharacter), ...characters.slice(0, lastCharacter)];
+    }
 
     return <section >
         <Flex direction="column"
@@ -25,15 +60,13 @@ function CharacterDisplay() {
                 borderColor={theme.colors.custom.dark }>
             <Heading as="h2">Characters</Heading>
             <Flex>
-                {
-                    characters.filter((character, index) => index < 4).map((character) => {
-                        return <CharacterCard key={character.character.id} character={character} />
-                    })
-                }
+                {getCards().map((character) => (
+                        <CharacterCard key={character.character.id} character={character} />
+                    ))}
             </Flex>
             <ButtonGroup variant="outline" spacing="80%" mt={4}>
-                <Button borderColor={theme.colors.custom[700]} leftIcon={<ArrowLeftIcon />} >Prev</Button>
-                <Button borderColor={theme.colors.custom[700]} rightIcon={<ArrowRightIcon />} >Next</Button>
+                <Button onClick={handlePrevious} borderColor={theme.colors.custom[700]} leftIcon={<ArrowLeftIcon />} >Prev</Button>
+                <Button onClick={handleNext} borderColor={theme.colors.custom[700]} rightIcon={<ArrowRightIcon />} >Next</Button>
             </ButtonGroup>
         </Flex>
     </section>
